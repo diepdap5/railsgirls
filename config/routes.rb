8648af5dbe30c4a778ece0675b98eab72b6e511c
diepdap5 <diepdap5@gmail.com>
2018-04-22 06:49:34 +0000

    Almost last

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments
 
-  resources :ideas, only: [:show, :index]
+  resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :user, only: [:edit, :update]
 end
